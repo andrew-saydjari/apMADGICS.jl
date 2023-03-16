d45dad8279fd526726e6a42633363e89b1871975
Andrew Saydjari <saydjari@g.harvard.edu>
2023-03-16 00:55:46 -0400

    Update pipeline.jl

--- src/pipeline.jl
+++ src/pipeline.jl
@@ -1,7 +1,6 @@
 ## This is the main pipeline that will batch over APOGEE files
 import Pkg
 Pkg.activate("/uufs/chpc.utah.edu/common/home/u6039752/scratch/julia_env/apMADGICS/")
-push!(LOAD_PATH,"./")
 
 using Distributed, SlurmClusterManager, LibGit2
 # addprocs(SlurmManager())
@@ -13,6 +12,7 @@ flush(stdout)
 @everywhere begin
     import Pkg
     Pkg.activate("/uufs/chpc.utah.edu/common/home/u6039752/scratch/julia_env/apMADGICS/")
+    push!(LOAD_PATH,"./")
 end
 
 @everywhere begin
