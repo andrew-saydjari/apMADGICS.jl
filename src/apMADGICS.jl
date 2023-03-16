6bbb78a594760a5673c0104965c112a99bd0b880
Andrew Saydjari <saydjari@g.harvard.edu>
2023-03-16 01:44:47 -0400

    Update apMADGICS.jl

--- src/apMADGICS.jl
+++ src/apMADGICS.jl
@@ -1,6 +1,8 @@
 module apMADGICS
 
     using LowRankOps, EllipsisNotation, ShiftedArrays, FITSIO, HDF5
+    using Distributed, SlurmClusterManager, LibGit2, Interpolations, SparseArrays
+    using StatsBase, LinearAlgebra, ProgressMeter
 
     # At some point, we should consider making this a real module, not just a repo of function definitions
     # I should at the very least harmonizing these usings with the pipeline script so that people can use the enviornment in the repo
