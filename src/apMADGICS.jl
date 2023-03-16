f1a1999eac373196fc56f183daa5e67a465ca082
Andrew Saydjari <saydjari@g.harvard.edu>
2023-03-16 01:08:29 -0400

    Update apMADGICS.jl

--- src/apMADGICS.jl
+++ src/apMADGICS.jl
@@ -2,7 +2,10 @@ module apMADGICS
 
     using LowRankOps, EllipsisNotation, ShiftedArrays
 
-    export cache_skyname
+    export cache_skyname, getSky4visit, cache_starname, stack_out, nanmedian, LowRankMultMatIP
+    export sampler_1d_hierarchy_var, update_Ctotinv_Vstarstarlines, deblend_components_all
+    export sampler_2d_hierarchy_var, shift_trim_range, sample_chi2_flux_dflux, marginalize_flux_err
+    export update_Ctotinv_Vdib, deblend_components_all_asym, nanify
 
     include("utils.jl")
     include("gridSearch.jl")
