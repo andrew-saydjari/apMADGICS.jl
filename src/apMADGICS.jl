48d48fa2808e56a2da256e2ef2a42a76c0415081
Andrew Saydjari <saydjari@g.harvard.edu>
2023-03-15 21:50:46 -0400

    Update apMADGICS.jl

--- src/apMADGICS.jl
+++ src/apMADGICS.jl
@@ -1,5 +1,16 @@
 module apMADGICS
 
-    using LowRankOps, EllipsisNotation
+    using LowRankOps, EllipsisNotation, ShiftedArrays
+
+    include("utils.jl")
+    include("gridSearch.jl")
+    include("componentAndPosteriors.jl")
+    include("fileNameHandling.jl")
+    include("ingest.jl")
+    include("lowRankPrescription.jl")
+    include("lowRankMultMatIP.jl")
+    include("marginalizeEW.jl")
+    include("spectraInterpolation.jl")
+    include("chi2Wrappers.jl")
 
 end
