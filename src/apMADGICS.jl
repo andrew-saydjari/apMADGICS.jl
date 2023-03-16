a380d06608bc82874baf1303828b54d1d04e257f
Andrew Saydjari <saydjari@g.harvard.edu>
2023-03-16 01:45:03 -0400

    Update apMADGICS.jl

--- src/apMADGICS.jl
+++ src/apMADGICS.jl
@@ -5,6 +5,5 @@ module apMADGICS
     using StatsBase, LinearAlgebra, ProgressMeter
 
     # At some point, we should consider making this a real module, not just a repo of function definitions
-    # I should at the very least harmonizing these usings with the pipeline script so that people can use the enviornment in the repo
 
 end
