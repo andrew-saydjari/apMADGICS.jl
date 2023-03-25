9578ac4532eaf50d9a6a8b15453eda25ba52294f
Andrew Saydjari <saydjari@g.harvard.edu>
2023-03-25 14:57:27 -0600

    use the include calls

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,15 @@
 using apMADGICS
 using Test
 
+src_dir = "../"
+include(src_dir*"src/utils.jl")
+include(src_dir*"src/gridSearch.jl")
+include(src_dir*"src/componentAndPosteriors.jl")
+include(src_dir*"src/fileNameHandling.jl")
+include(src_dir*"src/ingest.jl")
+include(src_dir*"src/lowRankPrescription.jl")
+include(src_dir*"src/marginalizeEW.jl")
+include(src_dir*"src/spectraInterpolation.jl")
+include(src_dir*"src/chi2Wrappers.jl")
+
 include("gridSearch.jl")
