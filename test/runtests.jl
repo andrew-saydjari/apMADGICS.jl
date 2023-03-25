aa019b2503a33f3546a7e51764d0fb19e28aefa2
Andrew Saydjari <saydjari@g.harvard.edu>
2023-03-25 15:02:34 -0600

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,8 +1,8 @@
 using apMADGICS #Why isn't LowRankOps loaded here?
 using Test
 
-using FITSIO, Serialization, HDF5, LowRankOps, EllipsisNotation, ShiftedArrays, Interpolations, SparseArrays, ParallelDataTransfer
-using StatsBase, LinearAlgebra, ProgressMeter
+using FITSIO, HDF5, LowRankOps, EllipsisNotation, ShiftedArrays, Interpolations, SparseArrays, ParallelDataTransfer
+using StatsBase, LinearAlgebra, ProgressMeter #Serialization
 
 src_dir = "../"
 include(src_dir*"src/utils.jl")
