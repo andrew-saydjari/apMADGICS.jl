a54786e9b7d8e83be93c85da678936c9a766ced1
Andrew Saydjari <saydjari@g.harvard.edu>
2023-03-28 00:06:37 -0400

    local scope uprng

--- src/gridSearch.jl
+++ src/gridSearch.jl
@@ -153,8 +153,7 @@ function sampler_1d_hierarchy_var(chi2_fun,lvltup;minres=1//10,stepx=1)
     global_minInd = 1
     global_flag = 0
     for (lvlind,lvl) in enumerate(lvltup)
-        
-        uprng = lvl
+        local uprng = lvl
         if lvlind != 1
             uprng = shift_range(lvl,round_step(global_minVal_interp,minres))
         end
@@ -244,9 +243,9 @@ function sampler_2d_hierarchy_var(chi2_fun,lvltup;step1=1,step2=1,minres1=1//10,
     global_minChi = 0
     global_minInd = CartesianIndex(1,1)
     global_flag = 0
-    for (lvlind,lvl) in enumerate(lvltup)
-        uprng1 = lvl[1]
-        uprng2 = lvl[2]
+    for (lvlind,lvl) in enumerate(lvltup)  
+        local uprng1 = lvl[1]
+        local uprng2 = lvl[2]
         if lvlind != 1
             uprng1 = shift_range(lvl[1],round_step(global_minVal_interp1,minres1))
             uprng2 = shift_trim_range(lvl[2],round_step(global_minVal_interp2,minres2))
