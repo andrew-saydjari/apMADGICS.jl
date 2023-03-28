2144e944788b8380e226a74a8e3535cb153e8dbc
Andrew Saydjari <saydjari@g.harvard.edu>
2023-03-28 16:08:32 -0600

    add Rinv test

--- test/gaussTests.jl
+++ test/gaussTests.jl
@@ -64,4 +64,41 @@
     @test abs(sum(filter(.!isnan,(dat2plot2.+dat2plot3).-dat2plot1))) < 1e-10
     @test abs(sum(x_comp_lst_asym[2].-x_data_clean)) < 1e-2
     
+    ## Gaussian interpolation linear->log with missing pixels/chip gaps
+    waveobs = 15220:0.1:15320
+    wavemodel = 10 .^(log10(15220):3e-6:log10(15320))
+    minw, maxw = extrema(wavemodel)
+    amp = -0.1
+    rng = MersenneTwister(2035)
+    function std_fxn(x;noiseFac=5e-3)
+        return noiseFac*(1+0.001*(x-15220)/(15320-15220))
+    end
+    var_vec = (std_fxn.(waveobs)).^2
+    x_data_clean = gauss1d(amp,x0,sigma,waveobs)
+    x_noise_vec = sqrt.(var_vec).*randn(rng,length(waveobs));
+    x_data = x_data_clean .+ x_noise_vec;
+
+    x_targ = gauss1d(amp,x0,sigma,wavemodel)
+
+    simplemsk = ones(Bool,length(waveobs))
+    simplemsk[560:620] .= false
+    simplemsk[500:501] .= false;
+    
+    simplemsk[500:501] .= false;
+
+    fullBit = zeros(Int,length(waveobs));
+    fullBit[1:559] .+= 2^1 # theoretical chip1
+    fullBit[621:end] .+= 2^2 # theoretical chip2
+    fullBit[500:501] .+= 2^4 # bad pixels
+
+    Xd_obs = x_data[simplemsk]; 
+    wave_obs = waveobs[simplemsk]
+    obsBit = fullBit[simplemsk]
+
+    Rinv = generateInterpMatrix_sparse_inv(wave_obs,obsBit,wavemodel,pixindx)
+    normvec = dropdims(sum(Rinv,dims=2),dims=2)
+    propmsk = (normvec.!=0)
+    
+    @test maximum((Rinv*x_data_clean[simplemsk].-x_targ)[propmsk]) .< 3e-5
+    @test sum((Rinv*x_data_clean[simplemsk].-x_targ)[propmsk]) .< 5e-5
 end
