a3bfc66f0c07ca08629843dca76ab613c59790c3
andrew-saydjari <saydjari@g.harvard.edu>
2023-05-28 21:03:13 -0600

    rescale ddCor run

--- pipeline.jl
+++ pipeline.jl
@@ -86,8 +86,9 @@ flush(stdout)
     global V_subpix_refLSF = alpha*read(f["Vmat"])
     close(f)
 
+    beta = 10;
     f = h5open(prior_dir2*"2023_05_25/APOGEE_starCor_svd_50_subpix.h5")
-    global V_subpix_cor = read(f["Vmat"])
+    global V_subpix_cor = beta*read(f["Vmat"])
     global msk_starCor = convert.(Bool,read(f["msk_starCor"]))
     close(f)
 
