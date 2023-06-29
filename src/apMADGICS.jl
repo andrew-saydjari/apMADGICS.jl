3b71219015ec14d34c4af0d4c294062f5109cce5
andrew-saydjari <saydjari@g.harvard.edu>
2023-06-29 16:49:11 -0600

    fix offset arrays

--- src/apMADGICS.jl
+++ src/apMADGICS.jl
@@ -3,7 +3,7 @@ module apMADGICS
     using LowRankOps, EllipsisNotation, ShiftedArrays, FITSIO, HDF5, ParallelDataTransfer
     using Distributed, SlurmClusterManager, LibGit2, Interpolations, SparseArrays
     using StatsBase, LinearAlgebra, ProgressMeter
-    using AstroTime, ImageFiltering
+    using AstroTime, ImageFiltering, OffsetArarys
 
     # At some point, we should consider making this a real module, not just a repo of function definitions
 
