cf3537f201f71ae02fd08cf495b510ffbf4a0454
andrew-saydjari <saydjari@g.harvard.edu>
2023-06-29 14:03:53 -0600

    fix repeated LibGit2 call

--- pipeline.jl
+++ pipeline.jl
@@ -43,7 +43,6 @@ using LibGit2
 git_dir = src_dir
 git_commit = LibGit2.head(git_dir)
 git_repo = LibGit2.GitRepo(git_dir)
-git_head = LibGit2.head(git_repo)
 git_branch = LibGit2.shortname(git_head)
 println("Running on branch: $git_branch, commit: $git_commit")
 flush(stdout)
