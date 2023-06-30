de16cd0f37433932e5050a1cadcfa46180f4ef43
andrew-saydjari <saydjari@g.harvard.edu>
2023-06-29 20:51:06 -0600

    refix LibGit2

--- pipeline.jl
+++ pipeline.jl
@@ -43,6 +43,7 @@ using LibGit2
 git_dir = src_dir
 git_commit = LibGit2.head(git_dir)
 git_repo = LibGit2.GitRepo(git_dir)
+git_head = LibGit2.head(git_repo)
 git_branch = LibGit2.shortname(git_head)
 println("Running on branch: $git_branch, commit: $git_commit")
 flush(stdout)
