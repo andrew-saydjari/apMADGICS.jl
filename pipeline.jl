e2fde8831e17b1a69eda43d4ed7dc5b6c4d1e643
andrew-saydjari <saydjari@g.harvard.edu>
2023-07-21 14:45:17 -0600

    try sky2 run

--- pipeline.jl
+++ pipeline.jl
@@ -322,7 +322,7 @@ end
                 chebmsk_exp = convert.(Bool,read(f["chebmsk_exp"]))
                 close(f)
 
-                f = h5open(prior_dir2*"2023_07_16/sky_priors/APOGEE_skyline_svd_120_f"*lpad(adjfibindx,3,"0")*".h5") #revert temp
+                f = h5open(prior_dir2*"2023_07_18/APOGEE_skyLineCorHcat_sky2_svd_180_f"*lpad(adjfibindx,3,"0")*".h5")
                 global V_skyline = read(f["Vmat"])
                 submsk = convert.(Bool,read(f["submsk"]))
                 close(f)
