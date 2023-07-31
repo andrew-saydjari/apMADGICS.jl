69e5120939604e11f775c0412c1cb42816b95bc7
andrew-saydjari <saydjari@g.harvard.edu>
2023-07-31 09:56:14 -0600

    fix covellipse tests

--- test/utils.jl
+++ test/utils.jl
@@ -56,6 +56,11 @@
     n_ellipse_vertices = 100
     # Calculate ellipse points using covellipse
     ellipse_points = covellipse(Σ, μ=μ, n_std=n_std, n_ellipse_vertices=n_ellipse_vertices)
+    
+    # Calculate ellipse points using the formula for an ellipse with identity covariance matrix
+    θ = range(0, 2π; length=n_ellipse_vertices)
+    x = μ[1] .+ n_std * cos.(θ)
+    y = μ[2] .+ n_std * sin.(θ);
     @test isapprox(ellipse_points[1], x)
     @test isapprox(ellipse_points[2], y)
 
@@ -65,19 +70,13 @@
     n_ellipse_vertices = 100
     # Calculate ellipse points using covellipse
     ellipse_points = covellipse(Σ, μ=μ, n_std=n_std, n_ellipse_vertices=n_ellipse_vertices)
-    @test isapprox(ellipse_points[1], x)
-    @test isapprox(ellipse_points[2], y)
-
+    
     # Calculate ellipse points using the formula for an ellipse with identity covariance matrix
     θ = range(0, 2π; length=n_ellipse_vertices)
     x = μ[1] .+ n_std * 2.0 * cos.(θ)
     y = μ[2] .+ n_std * 2.0 *sin.(θ);
-
-
-    # Calculate ellipse points using the formula for an ellipse with identity covariance matrix
-    θ = range(0, 2π; length=n_ellipse_vertices)
-    x = μ[1] .+ n_std * cos.(θ)
-    y = μ[2] .+ n_std * sin.(θ);
+    @test isapprox(ellipse_points[1], x)
+    @test isapprox(ellipse_points[2], y)
 
     lvl1 = -70:1//2:70
     lvl2 = -8:2//10:8
@@ -99,6 +98,7 @@
 
     # cpu_lock test (rough)
     # @test_throws nothing slurm_cpu_lock()
+    using ThreadPinning
     slurm_cpu_lock()
 
     z = v2z(10)
