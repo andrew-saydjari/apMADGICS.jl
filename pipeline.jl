7ae8490dcae56dd322b681624f636d34c4431f4d
andrew-saydjari <saydjari@g.harvard.edu>
2023-08-23 17:03:32 -0600

    skyoff test

--- pipeline.jl
+++ pipeline.jl
@@ -134,7 +134,7 @@ end
 end
 
 @everywhere begin
-    function pipeline_single_spectra(argtup; caching=true, sky_caching=true, sky_off=false, cache_dir="../local_cache", inject_cache_dir=prior_dir2*"2023_08_22/inject_local_cache")
+    function pipeline_single_spectra(argtup; caching=true, sky_caching=true, sky_off=true, cache_dir="../local_cache", inject_cache_dir=prior_dir2*"2023_08_22/inject_local_cache")
         ival = argtup[1]
         intup = argtup[2:end]
         out = []
@@ -469,7 +469,7 @@ iterlst = []
 Base.length(f::Iterators.Flatten) = sum(length, f.it)
 
 for adjfibindx=295:295 #, 245
-    subiter = deserialize(prior_dir2*"2023_08_22/injectNoDIB2/injection_input_lst_"*lpad(adjfibindx,3,"0")*".jdat")
+    subiter = deserialize(prior_dir2*"2023_08_22/injectNoDIBSky/injection_input_lst_"*lpad(adjfibindx,3,"0")*".jdat")
     subiterpart = Iterators.partition(subiter,batchsize)
     push!(iterlst,subiterpart)
 end
