be6646537a3c47cf31077ff84a678141083e3e41
andrew-saydjari <saydjari@g.harvard.edu>
2024-01-19 15:33:12 -0700

    verify_drp updates

--- src/verify_drp.jl
+++ src/verify_drp.jl
@@ -4,7 +4,7 @@ import Pkg
 using InteractiveUtils; versioninfo()
 Pkg.activate("../"); Pkg.instantiate(); Pkg.precompile()
 
-using FITSIO, StatsBase, ProgressMeter, Distributed, Serialization, Glob
+using FITSIO, StatsBase, ProgressMeter, Distributed, Serialization, Glob, DelimitedFiles
 
 src_dir = abspath("./")
 include(src_dir*"/fileNameHandling.jl")
@@ -17,8 +17,13 @@ end
 release_dir = "sdsswork/mwm"
 redux_ver = "1.2"
 
+release_dir_n = replace(release_dir,"/"=>"_")
+redux_ver_n = replace(redux_ver,"."=>"_")
+
 check_ap1d = false
-check_apCframes = true
+check_apCframes = false
+check_exp = false
+check_flux = true
 
 ### Ingest allVisit File
 tele_try_list = ["apo25m","lco25m"]
@@ -62,11 +67,15 @@ addprocs(32)
     import Pkg
     Pkg.activate("../")
 
-    using FITSIO, StatsBase, ProgressMeter, Distributed, Serialization, Glob
+    using FITSIO, StatsBase, ProgressMeter, Distributed, Serialization, Glob, DelimitedFiles
 
     src_dir = abspath("./")
     include(src_dir*"/fileNameHandling.jl")
 end
+
+println("##########################################################################################")
+println("############## Checking APOGEE DRP $(release_dir), version $(redux_ver) products ####################")
+println("##########################################################################################")
 
 ### Get Stars to Run (write apMADGICS.jl run files)
 ### Check Last Entry of ap1D Files
@@ -139,9 +148,9 @@ if check_ap1d
                 subiter = Iterators.zip(1:nspectra,repeat([release_dir],nspectra),repeat([redux_ver],nspectra),TELESCOPE[mskfib],FIELD[mskfib],string.(parse.(Int,PLATE[mskfib])),MJD[mskfib],fiber*ones(Int,nspectra))
                 tout = pmap(ap1D_check,subiter)
                 outcheck = convert(Vector{Bool},vcat(tout...))
-                serialize(outdir*"star_input_lst_"*lpad(adjfibindx,3,"0")*".jdat",collect(subiter)[outcheck])
+                serialize(outdir*"$(release_dir_n)_$(redux_ver_n)_star_input_lst_"*lpad(adjfibindx,3,"0")*".jdat",collect(subiter)[outcheck])
                 if count(.!outcheck)>0
-                    serialize(outdir*"star_bad_lst_"*lpad(adjfibindx,3,"0")*".jdat",collect(subiter)[.!outcheck])
+                    serialize(outdir*"$(release_dir_n)_$(redux_ver_n)_star_badap1D_lst_"*lpad(adjfibindx,3,"0")*".jdat",collect(subiter)[.!outcheck])
                 end
                 push!(starlst,count(outcheck))
             end
@@ -167,7 +176,7 @@ if check_ap1d
 
     if length(badfilevec)>0
         println("Total bad ap1D files: ",length(badfilevec))
-        writedlm(outdir*"$(release_dir)_$(redux_ver)_bad_ap1D.txt",badfilevec,',')
+        writedlm(outdir*"$(release_dir_n)_$(redux_ver_n)_bad_ap1D.txt",badfilevec,',')
     end
 end
 
@@ -211,7 +220,197 @@ if check_apCframes
 
     if count(.!ocheck)>0
         println("Total bad apCframe files: ",count(.!ocheck))
-        writedlm(outdir*"$(release_dir)_$(redux_ver)_bad_apCframe.txt",fnames[.!ocheck],',')
+        writedlm(outdir*"$(release_dir_n)_$(redux_ver_n)_bad_apCframe.txt",fnames[.!ocheck],',')
+    end
+end
+
+@everywhere begin
+    function exp_check(intup)
+        runnum,release_dir,redux_ver,tele,field,plate,mjd,fiberindx=intup
+
+        plateFile = build_platepath(release_dir,redux_ver,tele,field,plate,mjd,"a")
+        frame_lst = getFramesFromPlate(plateFile)
+        firstVisitExp =  minimum(parse.(Int,frame_lst))
+        expFile = build_expPath(release_dir,redux_ver,tele,mjd)
+        if !isfile(expFile)
+            return 3
+        end
+        try
+            f = FITS(expFile)
+            MED = read(f[2],"MED")
+            close(f)
+            return 0
+        catch
+            return 1
+        end
+    end
+end
+
+run_lsts = []
+run_lens = []
+for telematch in tele_list
+    for fiber in 1:300
+        teleind = (telematch == "lco25m") ? 2 : 1
+        adjfibindx = (teleind-1)*300 + fiber
+        run_per_fiber = deserialize(outdir*"star_input_lst_"*lpad(adjfibindx,3,"0")*".jdat")
+        push!(run_lsts,run_per_fiber)
+        push!(run_lens,length(run_per_fiber))
+    end
+end
+run_lst = vcat(run_lsts...)
+
+if check_exp
+    println("##### Checking the MJDexp Files #####")
+    pout = @showprogress pmap(exp_check,run_lst)
+
+    if count((pout.!=0))>0
+        mjd_bad = unique(map(x->x[7],run_lst[pout.!=0]))
+        println("Missing exp files: ",length(mjd_bad))
+        println("Uknown errors for exp files: ",count(pout.!=0)-count((pout.==3)))
+        writedlm(outdir*"$(release_dir_n)_$(redux_ver_n)_bad_exp.txt",mjd_bad,',')
+    end
+end
+
+@everywhere begin
+    function test_apFlux(intup;ret_file=false)
+        runnum,release_dir,redux_ver,tele,field,plate,mjd,fiberindx=intup
+        
+        plateFile = build_platepath(release_dir,redux_ver,tele,field,plate,mjd,"a")
+        frame_lst = getFramesFromPlate(plateFile)
+        firstVisitExp =  minimum(parse.(Int,frame_lst))
+        expFile = build_expPath(release_dir,redux_ver,tele,mjd)
+        
+        if !isfile(expFile)
+            return 3
+        end
+        f = FITS(expFile)
+        EXPNUM = read(f[2],"NUM")
+        IMAGETYP = read(f[2],"IMAGETYP")
+        CARTID = read(f[2],"CARTID")
+        close(f)
+        
+        firstVisitInd = findfirst(EXPNUM.==firstVisitExp)
+        cartVisit = CARTID[firstVisitInd]
+
+        expIndex_before = findlast((IMAGETYP.=="DomeFlat") .& (EXPNUM .< firstVisitExp))
+        expIndex_after = findfirst((IMAGETYP.=="DomeFlat") .& (EXPNUM .> firstVisitExp))
+        
+        # This is great logic, if the DRP did not only do the first two in the FPI era
+        # I am leaving it and overriding it because I want to be able to revert to this
+        expIndex = if isnothing(expIndex_before) & isnothing(expIndex_after)
+            -1
+        elseif !isnothing(expIndex_before) && (CARTID[expIndex_before] == cartVisit)
+            expIndex_before
+        elseif !isnothing(expIndex_after) && (CARTID[expIndex_after] == cartVisit)
+            expIndex_after
+        else
+            -2
+        end
+        
+        for off in vcat(0,-1:-1:-3,1:3)
+            if (1 <= expIndex+off <= length(CARTID)) && isfile(build_apFluxPath(release_dir,redux_ver,tele,mjd,"a",EXPNUM[expIndex+off])) && (CARTID[expIndex+off] == cartVisit)
+                expIndex+=off
+                break
+            end
+        end
+        
+        # This is to handle bad DRP behavior upstream
+        if all(CARTID.==0)
+            allDomes = findall(IMAGETYP.=="DomeFlat")
+            testinds = allDomes[(allDomes .<= expIndex)]
+            for testind in testinds[end:-1:1]
+                fluxFile = build_apFluxPath(release_dir,redux_ver,tele,mjd,"a",EXPNUM[testind])
+                if isfile(fluxFile)
+                    expIndex = testind
+                    break
+                else
+                    expIndex = -3
+                end
+            end
+        end
+            
+        if expIndex < 0
+            return expIndex
+        end
+        expectDome = EXPNUM[expIndex]
+
+        allgood = 0
+        for chip in ["a","b","c"]
+            fluxFile = build_apFluxPath(release_dir,redux_ver,tele,mjd,chip,expectDome)
+            if !isfile(fluxFile)
+                if ret_file
+                    return fluxFile
+                else
+                    return 4
+                end
+            end
+            try
+                f = FITS(fluxFile)
+                thrpt = read(f[3])
+                lastExt = read(f[5])
+                close(f)
+                allgood = 0
+            catch
+                allgood = 1
+            end
+        end
+        return allgood
+    end
+
+    g(x) = test_apFlux(x,ret_file=true)
+
+    function divide_vector(input_vector, lengths)
+        if sum(lengths) != length(input_vector)
+            throw(ArgumentError("Sum of lengths must be equal to the length of the input vector"))
+        end
+        divided_vectors = []
+        start_indices = [0; cumsum(lengths)]
+        for i in 1:length(lengths)
+            push!(divided_vectors,input_vector[start_indices[i]+1:start_indices[i+1]])
+        end
+        return divided_vectors
+    end
+end
+
+if check_flux
+    println("##### Checking the apFlux Files #####")
+    pout = @showprogress pmap(test_apFlux,run_lst)
+    pout1 = @showprogress pmap(g,star_input);
+
+    if count((pout.!=0))>0
+        println("Visits lost to missing exp files: $(count(pout.==3)) , $(100*count(pout.==3)/length(pout))%")
+        println("Visits lost because no flat taken at all: $(count(pout.==-1)) , $(100*count(pout.==-1)/length(pout))%")
+        println("Visits lost because no flat taken with CARTID: $(count(pout.==-2)) , $(100*count(pout.==-2)/length(pout))%")
+        println("Visits lost to missing flux files: $(count((pout.==4).|(pout.==-3))) , $(100*count((pout.==4).|(pout.==-3))/length(pout))%")
+        println("Unknown errors for flux files: ",count(pout.!=0)-count((pout.==3) .| (pout.==4) .| (pout.==-1) .| (pout.==-2) .| (pout.==-3)))
+
+        writedlm(outdir*"$(release_dir_n)_$(redux_ver_n)_missing_apFlux.txt",pout1[(pout.==4).|(pout.==-3)],',')
+        
+        missFluxing = divide_vector(pout.!=0, run_lens)
+        global ind_loc = 1
+        for telematch in tele_list
+            for fiber in 1:300
+                teleind = (telematch == "lco25m") ? 2 : 1
+                adjfibindx = (teleind-1)*300 + fiber
+                serialize(outdir*"$(release_dir_n)_$(redux_ver_n)_star_msk_fluxing_lst_"*lpad(adjfibindx,3,"0")*".jdat",missFluxing[ind_loc])
+                global ind_loc+=1
+            end
+        end
+
+        mjd_bad = unique(map(x->x[7],run_lst[(pout.==4).|(pout.==-3)]))
+        println("MJDs with missing apFlux Files: ",length(mjd_bad))
+        writedlm(outdir*"$(release_dir_n)_$(redux_ver_n)_bad_mjd_missing_apFlux.txt",mjd_bad,',')
+    end
+end
+
+# apply  any other masking to the star list and generate final star lists
+for telematch in tele_list
+    for fiber in 1:300
+        teleind = (telematch == "lco25m") ? 2 : 1
+        adjfibindx = (teleind-1)*300 + fiber
+        star_input = deserialize(outdir*"$(release_dir_n)_$(redux_ver_n)_star_input_lst_"*lpad(adjfibindx,3,"0")*".jdat")
+        msk = deserialize(outdir*"$(release_dir_n)_$(redux_ver_n)_star_msk_fluxing_lst_"*lpad(adjfibindx,3,"0")*".jdat")
+        serialize(outdir*"$(release_dir_n)_$(redux_ver_n)_star_input_lst_msked"*lpad(adjfibindx,3,"0")*".jdat",star_input[.!msk])
     end
 end
 
