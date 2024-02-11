5e7b30cb162f8b3b015a2618f0a79b2f812cbbb3
andrew-saydjari <saydjari@g.harvard.edu>
2024-02-11 12:13:09 -0700

    general star run, all else fixed

--- pipeline.jl
+++ pipeline.jl
@@ -66,7 +66,7 @@ using LibGit2; git_branch, git_commit = initalize_git(src_dir); @passobj 1 worke
     close(f)
 
     alpha = 1;
-    f = h5open(prior_dir*"2024_02_11/moonLine_priors/APOGEE_solar_exact_1_subpix_th22500.h5")
+    f = h5open(prior_dir*"2023_08_22/starLine_priors/APOGEE_stellar_kry_50_subpix_th22500.h5")
     global V_subpix_refLSF = alpha*read(f["Vmat"])
     close(f)
 
@@ -351,7 +351,7 @@ end
                 close(f)
 
                 # can consider changing dimension at the full reduction stage
-                f = h5open(prior_dir*"2024_02_11/moonLine_priors/APOGEE_solar_exact_1_subpix_"*lpad(adjfibindx,3,"0")*".h5")
+                f = h5open(prior_dir*"2023_08_22/starLine_priors/APOGEE_stellar_kry_50_subpix_"*lpad(adjfibindx,3,"0")*".h5")
                 global V_subpix = alpha*read(f["Vmat"])
                 close(f)
                 if ddstaronly
