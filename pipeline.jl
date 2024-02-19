03275303d61c1e94983a79e2425b29a6e0887719
andrew-saydjari <saydjari@g.harvard.edu>
2024-02-19 10:11:59 -0700

    335 th run

--- pipeline.jl
+++ pipeline.jl
@@ -401,12 +401,12 @@ end
                 chebmsk_exp = convert.(Bool,read(f["chebmsk_exp"]))
                 close(f)
 
-                f = h5open(prior_dir*"2024_02_18/sky_priors/APOGEE_skyline_bright_svd_120_20_8_6_1en3_f"*lpad(adjfibindx,3,"0")*".h5") #revert temp
+                f = h5open(prior_dir*"2024_02_16/sky_priors/APOGEE_skyline_bright_svd_120_20_8_6_1en3_f"*lpad(adjfibindx,3,"0")*".h5") #revert temp
                 global V_skyline_bright = read(f["Vmat"])
                 submsk_bright = convert.(Bool,read(f["submsk"]))
                 close(f)
 
-                f = h5open(prior_dir*"2024_02_18/sky_priors/APOGEE_skyline_faint_svd_120_20_8_6_1en3_f"*lpad(adjfibindx,3,"0")*".h5") #revert temp
+                f = h5open(prior_dir*"2024_02_16/sky_priors/APOGEE_skyline_faint_svd_120_20_8_6_1en3_f"*lpad(adjfibindx,3,"0")*".h5") #revert temp
                 global V_skyline_faint = read(f["Vmat"])
                 submsk_faint = convert.(Bool,read(f["submsk"]))
                 close(f)
@@ -566,7 +566,7 @@ batchsize = 10 #40
 iterlst = []
 Base.length(f::Iterators.Flatten) = sum(length, f.it)
 
-for adjfibindx = 101:101 #1:600 #295, 245, 335
+for adjfibindx = 335:335 #1:600 #295, 245, 335, 101
     subiter = deserialize(prior_dir*"2024_01_19/outlists/dr17_dr17_star_input_lst_msked_"*lpad(adjfibindx,3,"0")*".jdat")
     subiterpart = Iterators.partition(subiter,batchsize)
     push!(iterlst,subiterpart)
