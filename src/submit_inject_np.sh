ca8abbb95d471b3bea2b4883ca848fd1e4eff59b
andrew-saydjari <saydjari@g.harvard.edu>
2024-03-01 16:52:13 -0700

    add submission script

---
+++ src/submit_inject_np.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#SBATCH --account=sdss-np
+#SBATCH --partition=sdss-shared-np
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=64
+
+#SBATCH --mem=0 #requesting all of the memory on the node
+
+#SBATCH --time=96:00:00
+#SBATCH --job-name=apMADGICS_inject
+#SBATCH --output=%x_%j.out
+#SBATCH --err=%x_%j.err
+
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=7155301634@vtext.com
+# ------------------------------------------------------------------------------
+
+echo $SLURM_JOB_NODELIST
+
+# export JULIA_NUM_GC_THREADS=1
+# export JULIA_NUM_THREADS=1
+# export JULIA_CPU_THREADS=1
+
+julia +1.10.0 inject_test.jl
+
+# Clean up logs and Report Timing
+formatted_time=$(printf '%dd %dh:%dm:%ds\n' $(($SECONDS/86400)) $(($SECONDS%86400/3600)) $(($SECONDS%3600/60)) $(($SECONDS%60)))
+echo "Job completed in $formatted_time"
+
+mkdir -p slurm_logs
+mv ${SLURM_JOB_NAME}_${SLURM_JOBID}.out slurm_logs/${SLURM_JOB_NAME}_${SLURM_JOBID}.out
+mv ${SLURM_JOB_NAME}_${SLURM_JOBID}.err slurm_logs/${SLURM_JOB_NAME}_${SLURM_JOBID}.err
